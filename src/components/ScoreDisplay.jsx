3d8cb75043802710f8ebfb35ab8a62d16015fcff
gtpWebDev <33732289+gtpWebDev@users.noreply.github.com>
2024-05-14 18:09:20 +0100

    Page content adjustments

--- src/components/ScoreDisplay.jsx
+++ src/components/ScoreDisplay.jsx
@@ -3,6 +3,11 @@ import styles from "./ScoreDisplay.module.css";
 const ScoreDisplay = ({ currentScore, topScore }) => {
   return (
     <div className={styles.display}>
+      <div>
+        Get points by clicking on an image but don't click on any more than
+        once!
+      </div>
+      <p style={{ fontSize: "10px" }}></p>
       <div>Current Score: {currentScore}</div>
       <div>Top Score: {topScore}</div>
     </div>
